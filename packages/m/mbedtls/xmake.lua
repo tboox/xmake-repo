ae0334dbe226f0e004165d6dfa5f9eef3976215d
ruki <waruqi@gmail.com>
2021-02-25 20:46:43 +0800

    Update xmake.lua

--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -4,7 +4,7 @@ package("mbedtls")
     set_description("An SSL library")
 
     set_urls("https://github.com/ARMmbed/mbedtls/archive/mbedtls-$(version).zip")
-
+ 
     add_versions("2.13.0", "6e747350bc13e8ff51799daa50f74230c6cd8e15977da55dd59f57b23dcf70a6")
     add_versions("2.7.6", "e527d828ab82650102ca8031302e5d4bc68ea887b2d84e43d3da2a80a9e5a2c8")
 
@@ -32,7 +32,7 @@ package("mbedtls")
         os.cp("library/libmbedx509.*", package:installdir("lib"))
     end)
 	
-    on_install("macosx", "linux", function (package)
+    on_install("macosx", "linux", "bsd", function (package)
         io.gsub("./Makefile", "DESTDIR=/usr/local", "DESTDIR=" .. package:installdir())
         import("package.tools.make").build(package)
         os.vrun("make install")
